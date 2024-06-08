2199a51606875271d64e6e3851ac9abc7307f117
brinkqiang <brink.qiang@gmail.com>
2024-06-08 14:11:25 +0800

    add

--- test/testattach/testattach.cpp
+++ test/testattach/testattach.cpp
@@ -5,27 +5,27 @@ using namespace std;
 
 int main()
 {
-  try {
-    sqlite3pp::database db("foods.db");
+	try {
+		sqlite3pp::database db("foods.db");
 
-    db.attach("test.db", "test");
-    {
-      sqlite3pp::transaction xct(db);
-      {
-	sqlite3pp::query qry(db, "SELECT epi.* FROM episodes epi, test.contacts con WHERE epi.id = con.id");
+		db.attach("test.db", "test");
+		{
+			sqlite3pp::transaction xct(db);
+			{
+				sqlite3pp::query qry(db, "SELECT epi.* FROM episodes epi, test.contacts con WHERE epi.id = con.id");
 
-	for (sqlite3pp::query::iterator i = qry.begin(); i != qry.end(); ++i) {
-	  for (int j = 0; j < qry.column_count(); ++j) {
-	    cout << (*i).get<char const*>(j) << "\t";
-	  }
-	  cout << endl;
+				for (sqlite3pp::query::iterator i = qry.begin(); i != qry.end(); ++i) {
+					for (int j = 0; j < qry.column_count(); ++j) {
+						cout << (*i).get<char const*>(j) << "\t";
+					}
+					cout << endl;
+				}
+				cout << endl;
+			}
+		}
 	}
-	cout << endl;
-      }
-    }
-  }
-  catch (exception& ex) {
-    cout << ex.what() << endl;
-  }
+	catch (exception& ex) {
+		cout << ex.what() << endl;
+	}
 
 }
